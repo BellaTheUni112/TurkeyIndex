46eb846f410b2a30ddee587b50e336747af10ea5
Turkey <bellatheunicorn521@gmail.com>
2026-04-22 02:38:32 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=torrents;charset=utf8mb4", "torrent", "skibidisigma");
+$pdo = new PDO("mysql:host=localhost;dbname=torrents;charset=utf8mb4", "torrent", "strongpasswordyoushouldchange");
 ?>
